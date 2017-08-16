a3e36594eb7f31b5390108d710cc3ca66849dbc3
Mikael Håkansson <wmmihaa@hotmail.com>
2017-08-16 16:56:08 +0200

    ...

--- snapstart.js
+++ snapstart.js
@@ -2,119 +2,100 @@
 
 var path = require("path");
 var os = require('os');
-var interval;
-var _imeiInterval;
-
+var url = require('url');
+var request = require("request");
 var debug = process.execArgv.find(function (e) { return e.startsWith('--debug'); }) !== undefined;
 
 // Load settings 
 var SettingsHelper = require("./lib/SettingsHelper.js");
 var settingsHelper = new SettingsHelper();
 
-if (!debug) {
-    var packagePath = settingsHelper.nodePackagePath;
+var packagePath = settingsHelper.nodePackagePath;
 
-    process.env.NODE_PATH = packagePath;
-    process.env.HOME = os.userInfo().homedir;
+process.env.NODE_PATH = packagePath;
+process.env.HOME = os.userInfo().homedir;
 
-    require('app-module-path').addPath(packagePath);
-    require('module').globalPaths.push(packagePath);
-}
+require('app-module-path').addPath(packagePath);
+require('module').globalPaths.push(packagePath);
 
 // If user hasn't logged in before we'll try to get the IMEI id
 if (settingsHelper.isFirstStart()) {
 
-    //if (debug) {
-    //    imei = "014752000022947";
-    //    tryLoginUsingICCID();
-    //    return;
-    //}
-
-    var exec = require('child_process').exec;
-
-    setInterval(function () {
-        tryGetIMEI(function (imei) {
-            if (!imei){
-                console.error("STARTSNAP: Unable to get IMEI");
-                return;
-            }
-            else {
-                console.log("STARTSNAP: Found IMEI: %s", imei);
-                clearInterval(_imeiInterval);
-                tryLoginUsingICCID();
-                interval = setInterval(function () {
-                    tryLoginUsingICCID(imei);
-                }, 6000);
-            }
-        })
-    }, 4000);
-
-
-    //var child;
-    //child = exec("sudo mmcli -m 0|grep -oE \"imei: '(.*)'\"|sed 's/imei: //g'|sed \"s/'//g\"", function (error, stdout, stderr) {
-    //    console.log('imei: ' + stdout);
-    //    if (error !== null) {
-    //        console.log("Unable to get the IMEI id");
-    //        console.log('ERROR: ' + error);
-    //        process.abort();
-    //    }
-    //    else {
-    //        imei = stdout;
-    //        tryLoginUsingICCID();
-    //        interval = setInterval(function () {
-    //            tryLoginUsingICCID();
-    //        }, 10000);
-    //    }
-    //});
+    var snapLoginHandler = new SnapLoginHandler(settingsHelper);
+    snapLoginHandler.start();
 }
 else {
     require("./start.js");
 }
 
-function tryGetIMEI(callback) {
-    exec("sudo mmcli -m 0|grep -oE \"imei: '(.*)'\"|sed 's/imei: //g'|sed \"s/'//g\"", function (error, stdout, stderr) {
-        console.log('STARTSNAP: imei: ' + stdout);
-        if (error !== null) {
-            console.log("STARTSNAP: Unable to get the IMEI id");
-            console.log('STARTSNAP: ERROR: ' + error);
-            callback(error, null);
-        }
-        else {
-            imei = stdout;
-            if (!imei)
-                callback(null);
-            else
+function SnapLoginHandler(settingsHelper) {
+    console.log("STARTSNAP: SnapLoginHandler started");
+    var self = this;
+    this.interval = null;
+    var count = 0;
+    this.start = function () {
+        this.interval = setInterval(function () {
+            tryGetIMEI(function (imei) {
+                if (imei) {
+                    clearInterval(self.interval);
+                    self.interval = setInterval(function () {
+                        tryLoginUsingICCID(imei, function (done) {
+                            if (done) {
+                                clearInterval(self.interval);
+                                console.log("done");
+                            }
+                        })
+                    }, 2000);
+                }
+            })
+        }, 2000);
+    }
+
+    function tryGetIMEI(callback) {
+        exec("sudo mmcli -m 0|grep -oE \"imei: '(.*)'\"|sed 's/imei: //g'|sed \"s/'//g\"", function (error, stdout, stderr) {
+            console.log('STARTSNAP: imei: ' + stdout);
+            if (error !== null) {
+                console.log("STARTSNAP: Unable to get the IMEI id");
+                console.log('STARTSNAP: ERROR: ' + error);
+
+                callback();
+            }
+            else {
+                imei = stdout;
                 callback(imei);
-        }
-    });
-}
-function tryLoginUsingICCID(imei) {
-    var url = require('url');
-    var request = require("request");
-    
+            }
+        });
+    }
+    function tryLoginUsingICCID(imei, callback) {
+        var hubUri = url.parse(settingsHelper.settings.hubUri);
 
-    if (debug)
-        process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
-    var hubUri = url.parse(settingsHelper.settings.hubUri);
-
-    var uri = 'https://' + hubUri.host + '/jasper/signInUsingICCID?iccid=' + imei;
-    console.log("STARTSNAP: calling jasper service..." + uri);
-    request.post({ url: uri, timeout: 5000 }, function (err, response, body) {
-        if (err)
-            console.error("STARTSNAP: ERROR: error: " + err);
-        else if (response.statusCode !== 200)
-            console.error("STARTSNAP: FAILED: response: " + response.statusCode);
-        else {
-            console.log("STARTSNAP: Got settings from microServiceBus.com. All good...");
-            clearInterval(interval);
-            var settings = JSON.parse(body);
-            settingsHelper.settings.id = settings.id;
-            settingsHelper.settings.nodeName = settings.nodeName;
-            settingsHelper.settings.organizationId = settings.organizationId;
-            settingsHelper.settings.sas = settings.sas;
-            settingsHelper.save();
-            require("./start.js");
-        }
-    })
+        var uri = 'https://' + hubUri.host + '/jasper/signInUsingICCID?iccid=' + imei;
+        console.log("STARTSNAP: calling jasper service..." + uri);
+        request.post({ url: uri, timeout: 5000 }, function (err, response, body) {
+            if (err) {
+                console.error("STARTSNAP: ERROR: error: " + err);
+                callback();
+                return;
+            }
+            else if (response.statusCode !== 200) {
+                console.error("STARTSNAP: FAILED: response: " + response.statusCode);
+                callback();
+                return;
+            }
+            else {
+                console.log("STARTSNAP: Got settings from microServiceBus.com. All good...");
+                console.log("STARTSNAP: _jasperInterval = " + _jasperInterval);
+                clearInterval(_jasperInterval);
+                var settings = JSON.parse(body);
+                settingsHelper.settings.id = settings.id;
+                settingsHelper.settings.nodeName = settings.nodeName;
+                settingsHelper.settings.organizationId = settings.organizationId;
+                settingsHelper.settings.sas = settings.sas;
+                settingsHelper.save();
+                console.log("STARTSNAP: Saved settings");
+                require("./start.js");
+                callback(true);
+            }
+        })
+    }
 }
