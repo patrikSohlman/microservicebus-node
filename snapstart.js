8f321d874ce31f62c79fa96666c55413e4957599
Mikael Håkansson <wmmihaa@hotmail.com>
2017-08-16 17:34:31 +0200

    ggg

--- snapstart.js
+++ snapstart.js
@@ -4,6 +4,7 @@ var path = require("path");
 var os = require('os');
 var url = require('url');
 var request = require("request");
+var exec = require('child_process').exec;
 var debug = process.execArgv.find(function (e) { return e.startsWith('--debug'); }) !== undefined;
 
 // Load settings 
